0d996544576b6f6390e6d8e42f95fd1cf1a2df09
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-04-10 12:06:54 +0530

    added initial cmd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello World!")
+use clap::{Parser,Subcommand,Args};
+
+
+#[derive(Parser)]
+#[command(version,about,long_about=None)]
+struct Cli {
+    #[command(subcommand)]
+    command : Commands
 }
+
+#[derive(Subcommand)]
+
+enum Commands {
+    
+    Add(AddArgs) ,
+    Remove,
+    Display,
+    Update
+
+}
+
+
+
+#[derive(Debug,Args)]
+struct AddArgs {
+    name : String,
+    description : String,
+    status : Status
+}
+
+
+#[derive(Debug,Clone,clap::ValueEnum)]
+enum Status {
+    COMPLETED,
+    PENDING,
+    PROGRESS,
+    NOTSTARTED
+}
+
+
+
+
+fn main()  {
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Add(todo) => {
+             println!("Add a Todo {:?}",todo);
+        }
+        Commands::Remove => {
+             println!("Remove a Todo");
+        }
+        Commands::Display => {
+             println!("Display all the Todo");
+        }
+        Commands::Update => {
+             println!("Update a Todo");
+        }
+    }
+}
+
